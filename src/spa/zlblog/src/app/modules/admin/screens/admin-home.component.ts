72f0b1d2c2fed83d616d6c1cdb1104f561aed189
Zongyi Li <blurlzy@hotmail.com>
2025-02-11 22:31:51 +1100

    upgraded from v18 to v19

---
+++ src/spa/zlblog/src/app/modules/admin/screens/admin-home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-home',
+  imports: [],
+  template: `
+    <p>
+      admin-home works!
+    </p>
+  `,
+  styles: ``
+})
+export class AdminHomeComponent {
+
+}
