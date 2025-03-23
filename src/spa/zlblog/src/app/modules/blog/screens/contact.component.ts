51ac2a174637ec6bdb8db2aeca95ec7c790ffc77
Zongyi Li <blurlzy@hotmail.com>
2025-03-23 20:19:44 +1100

    Added progress bar

--- src/spa/zlblog/src/app/modules/blog/screens/contact.component.ts
+++ src/spa/zlblog/src/app/modules/blog/screens/contact.component.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
 // services
 import { Util } from '../../../core/services/util.service';
+import { Loader } from '../../../core/services/loader.service';
+
 @Component({
   selector: 'app-contact',
   imports: [],
   template: `
   <div class="ratio ratio-4x3">
     <iframe
-      src="https://forms.office.com/Pages/ResponsePage.aspx?id=saBbiNVo4kCtqiOMtpnb3ONR4c4CD3RNjH4WjPfnf81UMlY5TDRPTkdBUE9UV1IySkFWMzRMMzBZSy4u">
+      src="https://forms.office.com/Pages/ResponsePage.aspx?id=saBbiNVo4kCtqiOMtpnb3ONR4c4CD3RNjH4WjPfnf81UMlY5TDRPTkdBUE9UV1IySkFWMzRMMzBZSy4u"
+      (load)="loader.isLoading.next(false)">
     </iframe>
   </div>
     `,
@@ -16,6 +19,7 @@ import { Util } from '../../../core/services/util.service';
 })
 export class ContactComponent {
   private readonly util = inject(Util);
+  public readonly loader = inject(Loader);
 
   ngOnInit(): void {
     // reset meta tags
@@ -23,5 +27,16 @@ export class ContactComponent {
 
     // ensure it scrolls to the top of the page
     window.scroll(0, 0);
+
+    // start loader
+    setTimeout(() => {
+      this.loader.isLoading.next(true);
+    });
+
+    // // ensure the loader is hidden after 5 seconds
+    // setTimeout(() => {
+    //   this.loader.isLoading.next(false);
+    // }, 5000);
   }
+
 }
